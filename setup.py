31a462d597df8888c52973ad5c18c0c04f0f0460
sugasakimaika <sakimaikawahaha@gmail.com>
2023-11-12 16:14:16 +0900

    Update settings

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="my_app_for_Womanmoneycareer",
+    version='1.0',
+    description='Pythonのディレクトリ構成のテスト用',
+    author='Maika Sugasaki',
+    author_email='sakimaikawahaha@gmail.com',
+    url='https://github.com/sugasakimaika/my_app_for_Womanmoneycareer',
+)
