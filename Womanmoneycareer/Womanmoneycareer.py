18f15e11ccf326afffd1d1978e0308751ea4d8d8
sugasakimaika <sakimaikawahaha@gmail.com>
2023-11-12 18:10:47 +0900

    Update settings

---
+++ Womanmoneycareer/Womanmoneycareer.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print('Hello, %s!' % name)
+
+def multiply(a, b):
+    return a * b
---
+++ tests/test_Womanmoneycareer.py
@@ -0,0 +1,5 @@
+from Womanmoneycareer import Womanmoneycareer
+
+
+def test_multiply():
+    assert Womanmoneycareer.multiply(2, 3) == 6
