997a7b347c9224ada41e26c7a297933217bbb5d2
sugasakimaika <sakimaikawahaha@gmail.com>
2023-11-19 16:23:14 +0900

    Update html

--- tests/test_Womanmoneycareer.py
+++ tests/test_Womanmoneycareer.py
@@ -3,3 +3,17 @@ from Womanmoneycareer import Womanmoneycareer
 
 def test_multiply():
     assert Womanmoneycareer.multiply(2, 3) == 6
+from Womanmoneycareer import Womanmoneycareer
+
+def test_multiply():
+    assert Womanmoneycareer.multiply(2, 3) == 6
+
+def test_hello():
+    # Test case 1: Check if the function returns the correct greeting
+    assert Womanmoneycareer.hello("Alice") == "Hello, Alice!"
+
+    # Test case 2: Check if the function returns the correct greeting for an empty string
+    assert Womanmoneycareer.hello("") == "Hello, !"
+
+    # Test case 3: Check if the function returns the correct greeting for a name with special characters
+    assert Womanmoneycareer.hello("@#$%^") == "Hello, @#$%^!"
