9b8e2ee7c6419504a18d9c1e63e735307d1344c5
sugasakimaika <sakimaikawahaha@gmail.com>
2023-11-20 21:32:59 +0900

    Update app

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ womanmoneycareer = Flask(__name__)
 
 @womanmoneycareer.route('/', methods=['GET', 'POST'])
 def index():
+    result = None
     if request.method == 'POST':
-        user_input = request.form['user_input']
-        # ここにロジックを実装
-        return render_template('index.html', result=user_input)
-    return render_template('index.html')
+        try:
+            user_input = float(request.form['user_input'])
+            result = calculate_tax(user_input)
+        except ValueError:
+            result = "無効な入力です。数値を入力してください。"
+
+    return render_template('index.html', result=result)
+
+def calculate_tax(amount):
+    tax_rate = 0.10  # 消費税率（10％）
+    return amount * (1 + tax_rate)
 
 if __name__ == '__main__':
     womanmoneycareer.run(debug=True)
